5881856cad82b90436c899f8b0ebbffa8dbc5d16
Ananthannn <160157709+Ananthannn@users.noreply.github.com>
2025-12-12 22:12:30 +0530

    added comments to the dataset.py file

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -4,38 +4,54 @@ from torch.utils.data import Dataset
 from tokenizers import Tokenizer
 
 class AutoRegressiveDataset(Dataset):
+    # Initialize datapath for autoregressive language modeling
     def __init__(self, clean_data_path: str, tokenizer_path: str, max_length: int = 512):
         self.max_length = max_length
 
+        # Load cleaned data lines
         with open(clean_data_path) as f:
+
+            # Remove empty lines
             self.lines = [lines.strip() for lines in f if lines.strip()]
 
+        # Load tokenizer from file
         self.tokenizer = Tokenizer.from_file(tokenizer_path)
 
+        # Get special token IDs
         self.BOS_id = self.tokenizer.token_to_id("[BOS]")
         self.EOS_id = self.tokenizer.token_to_id("[EOS]")
         self.PAD_id = self.tokenizer.token_to_id("[PAD]")
 
     def __len__(self):
+
+        # Return number of lines in dataset
         return len(self.lines)
     
     def __getitem__(self , idx):
         
+        # Get text line at index
         text = self.lines[idx]
 
+        # Tokenize text to IDs
         id = self.tokenizer.encode(text).ids
 
+        # Add BOS and EOS tokens
         id = [self.BOS_id] + id + [self.EOS_id]
 
+        # Truncate if longer than max length
         if len(id) > self.max_length:
             id = id[:self.max_length]
 
+        # Prepare input IDs, target IDs, and attention mask
         input_ids = id
 
+        # Shift target IDs by one position
         target_ids = id[1:] + [-100]
 
+        # Create attention mask (1 for real tokens)
         attention_mask = [1] * len( input_ids )
 
+        # return data sample as dictionary
         return {
 
             "input_ids": input_ids,
@@ -43,7 +59,7 @@ class AutoRegressiveDataset(Dataset):
             "attention_mask": attention_mask
         }
     
-
+# Collate function for DataLoader to pad sequences in batch for making it uniform length , so that torch can process it into tensors
 def collate_fn(batch):
 
     input_id = [data["input_ids"] for data in batch]
